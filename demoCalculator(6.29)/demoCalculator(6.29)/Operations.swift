ba59cdad4e874a693280634076f21fe03d2da566
Segeun Choi <chltprms77@icloud.com>
2023-07-04 21:39:13 +0900

    클론 스터디 활동 추가

---
+++ demoCalculator(6.29)/demoCalculator(6.29)/Operations.swift
@@ -0,0 +1,10 @@
+//
+//  Operations.swift
+//  demoCalculator(6.29)
+//
+//  Created by 최세근 on 2023/06/29.
+//
+
+
+import Foundation
+
---
+++ demoCalculator(6.29)/demoCalculator(6.29)/demoCalculator_6_29_App.swift
@@ -0,0 +1,17 @@
+//
+//  demoCalculator_6_29_App.swift
+//  demoCalculator(6.29)
+//
+//  Created by 최세근 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct demoCalculator_6_29_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
